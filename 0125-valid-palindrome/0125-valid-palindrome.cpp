abdc07688c818ac64bba8dae86e0117125344a7e
HIMAN2715 <himanshuthagele2710@gmail.com>
2026-05-27 17:46:03 +0530

    Time: 0 ms (100%), Space: 10.1 MB (36.49%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string clean = "";
+
+        for(int i=0 ; i<s.length();i++){
+            if(isalnum(s[i])){
+                clean+=tolower(s[i]);
+            }
+        }
+
+     
+        int left=0;
+        int right=clean.length()-1;
+        while(left<=right){
+            if(clean[left]==clean[right]){
+                left++;
+                right--;  
+            } 
+            else
+                return false ;
+        }
+      
+        return true;
+        
+    }
+};
